52ebf5003e27204fde877deb7be452edbf03df1d
jagery <jageryzhong@outlook.com>
2021-06-16 14:18:09 +0800

    dot11

--- dot_dream2lte.mk
+++ dot_dream2lte.mk
@@ -27,7 +27,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dream2lte
-PRODUCT_NAME := lineage_dream2lte
+PRODUCT_NAME := dot_dream2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G955F
 PRODUCT_MANUFACTURER := samsung
