3abec13cd2d44ac17cc8fda16f79b1977569db2f
jagery <jageryzhong@outlook.com>
2021-06-19 00:04:59 +0800

    dot11
    Change-Id: I53966d2eb2f9f9f82571ef7fd61b1af5d150654e

--- dot_dream2lte.mk
+++ dot_dream2lte.mk
@@ -23,7 +23,7 @@ $(call inherit-product, device/samsung/dream2lte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dream2lte
